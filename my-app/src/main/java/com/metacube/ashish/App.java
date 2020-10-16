2ad7e92e82dcd5a180c262c0d098da98e95e7974
Ashish Mishra <ashish.mishra@metacube.com>
2020-10-16 14:54:57 +0530

    project files added

---
+++ my-app/src/main/java/com/metacube/ashish/App.java
@@ -0,0 +1,13 @@
+package com.metacube.ashish;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! By Ashish" );
+    }
+}
---
+++ my-app/src/test/java/com/metacube/ashish/AppTest.java
@@ -0,0 +1,20 @@
+package com.metacube.ashish;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for sample application.
+ */
+public class AppTest 
+{
+    /**
+     *  Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
